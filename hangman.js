0beb0b77c75ae2b532af30ad01e0f07c3f1291c8
tani2110 <137906236+tani2110@users.noreply.github.com>
2023-07-26 21:23:14 +0530

    Add files via upload
    added

---
+++ hangman.js
@@ -0,0 +1,74 @@
+"use strict";
+
+const guessWord = document.querySelector(".guess-word");
+const letters = document.querySelectorAll(".letters");
+const livesElement = document.querySelector("#lives");
+
+let lives = 9;
+const alphabetGrid = document.querySelector(".alphabet-grid");
+let guessedLetters = [];
+let charArray = [
+  "dolphin",
+  "notebook",
+  "orange",
+  "papaya",
+  "calculator",
+  "mango",
+  " hyderabad",
+  "cake",
+];
+let wordSelected = charArray[Math.floor(Math.random() * 8)];
+console.log(wordSelected);
+for (let i = 0; i < wordSelected.length; i++) {
+  const HTML = `<span class= "dashWord"> _ &nbsp </span>`;
+  guessWord.insertAdjacentHTML("beforeend", HTML);
+}
+
+function displayWord() {
+  guessWord.innerHTML = "";
+
+  for (let i = 0; i < wordSelected.length; i++) {
+    const letter = wordSelected[i];
+
+    const HTML = `<span class="dashWord">${
+      guessedLetters.includes(letter) ? letter : "_" + "&nbsp"
+    }</span>`;
+
+    guessWord.insertAdjacentHTML("beforeend", HTML + " ");
+  }
+}
+
+function letterPress(event) {
+  const checkLetter = event.target.innerHTML.toLowerCase();
+  event.target.style.color = "red ";
+  console.log(checkLetter);
+  guessedLetters.push(checkLetter);
+
+  if (wordSelected.includes(checkLetter)) {
+    displayWord();
+
+    let flag = true;
+    const spanElements = guessWord.querySelectorAll("span.dashWord");
+    for (let i = 0; i < wordSelected.length; i++) {
+      if (spanElements[i].textContent.trim() === "_") {
+        //  if (guessWord[i] === "_ ") {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) {
+      livesElement.textContent = "You Won! ";
+    }
+  } else {
+    lives--;
+    if (lives === 0) {
+      livesElement.textContent = "You Lost :( ";
+    } else {
+      livesElement.textContent = `Lives: ${lives}`;
+    }
+  }
+}
+
+letters.forEach(function (i) {
+  i.addEventListener("click", letterPress);
+});
